0149dc0c782c843de56ef0b177fe2b7feec40cd8
mempooler <mempooler@protonmail.com>
2023-03-07 12:45:51 -0600

    tmp

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 
 func main() {
 	filename := flag.String("f", "", "file to tail")
+	lines := flag.Int("n", 10, "number of lines to tail")
 	flag.Parse()
 
 	if *filename == "" {
@@ -19,8 +20,11 @@ func main() {
 	}
 
 	t, err := tail.TailFile(*filename, tail.Config{
-		Location: &tail.SeekInfo{Whence: os.SEEK_END},
-		Follow:   true,
+		Location: &tail.SeekInfo{
+			Offset: -n,
+			Whence: os.SEEK_END,
+		},
+		Follow: true,
 	})
 	if err != nil {
 		panic(err)
