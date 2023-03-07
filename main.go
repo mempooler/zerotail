5402b06781b0cf854e671888fe5c299c3d35afc6
mempooler <mempooler@protonmail.com>
2023-03-06 23:42:09 -0600

    tmp

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
 
+	"github.com/hpcloud/tail"
 	"github.com/mempooler/zerolog"
 	"github.com/mempooler/zerolog/log"
 )
@@ -20,25 +20,15 @@ func main() {
 		os.Exit(1)
 	}
 
-	file, err := os.Open(*filename)
-	if err != nil {
-		log.Fatal().Err(err).Msg("failed to open file")
-	}
-	defer file.Close()
-
-	file.Seek(0, os.SEEK_END)
-	scanner := bufio.NewScanner(file)
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
-	for scanner.Scan() {
+	t, err := tail.TailFile(*filename, tail.Config{Follow: true})
+	for line := range t.Lines {
 		var fields map[string]interface{}
-		if err := json.Unmarshal(scanner.Bytes(), &fields); err != nil {
-			log.Error().Err(err).Msgf("failed to parse line: %q", scanner.Text())
+		if err := json.Unmarshal([]byte(line.Text), &fields); err != nil {
+			log.Error().Err(err).Msgf("failed to parse line: %q", line.Text)
 			continue
 		}
 
 		log.Log().Fields(fields).Msg("")
-	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal().Err(err).Msg("failed to read from file")
 	}
 }
